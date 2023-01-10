eac41d40ca354e022d9cd826c07cb3044a51849b
OlgaMunaieva <asyssync@gmail.com>
2023-01-10 12:47:52 +0200

    task-01 done

---
+++ js/task-01.js
@@ -0,0 +1,10 @@
+const categoriesEl = document.querySelectorAll("li.item");
+console.log("Number of categories: ", categoriesEl.length);
+
+for (const categoryEl of categoriesEl) {
+  console.log("Category: ", categoryEl.firstElementChild.textContent);
+  console.log(
+    "Elements: ",
+    categoryEl.lastElementChild.querySelectorAll("ul li").length
+  );
+}
