3f6fe0dbd2d2487491cf7a30069dd8da9184f861
OlgaMunaieva <asyssync@gmail.com>
2023-01-10 14:23:28 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,23 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsEL = ingredients.map((ingredient) => {
+  const itemEl = document.createElement("li");
+  itemEl.textContent = ingredient;
+  itemEl.setAttribute("class", "item");
+  // console.log(itemEl);
+  return itemEl;
+});
+// console.log(ingredientsEL);
+
+const listEl = document.querySelector("#ingredients");
+
+listEl.append(...ingredientsEL);
+
+// console.log(document);
