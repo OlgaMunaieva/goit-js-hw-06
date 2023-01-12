857d4a86cf9d779a6506814f9df33b59dee58315
OlgaMunaieva <asyssync@gmail.com>
2023-01-12 16:39:22 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const buttonEl = document.querySelector(".change-color");
+const bodyEl = document.querySelector("body");
+const showColor = document.querySelector(".color");
+
+buttonEl.addEventListener("click", () => {
+  const color = getRandomHexColor();
+  bodyEl.style.backgroundColor = color;
+  showColor.textContent = color;
+});
