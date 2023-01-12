641b28ff1dc29add741e86a8cf05135fff70526f
OlgaMunaieva <asyssync@gmail.com>
2023-01-12 14:49:26 +0200

    Update task-07.js

---
+++ js/task-07.js
@@ -0,0 +1,7 @@
+const inputEl = document.querySelector("#font-size-control");
+
+const text = document.querySelector("#text");
+
+inputEl.addEventListener("input", (event) => {
+  text.style.fontSize = `${event.currentTarget.value}px`;
+});
