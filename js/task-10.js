1ca99f451d0d73814a2b26fd9a32b60e4f7fb2b3
OlgaMunaieva <asyssync@gmail.com>
2023-01-13 13:22:48 +0200

    first part of task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,44 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const collectionCreationBlock = document.getElementById("controls");
+console.dir(collectionCreationBlock);
+
+const inputEl = collectionCreationBlock.children[0];
+console.log(inputEl);
+
+const buttonCreate = collectionCreationBlock.children[1];
+console.log(buttonCreate);
+
+const buttonDestroy = collectionCreationBlock.children[2];
+console.log(buttonDestroy);
+
+const firstBoxSize = 30;
+const increaseNextBoxes = 10;
+
+const boxesEl = document.getElementById("boxes");
+
+inputEl.addEventListener("change", (event) => {
+  const amount = event.currentTarget.value;
+  console.log(amount);
+  createBoxes(amount);
+});
+
+function createBoxes(amount) {
+  let boxes = [];
+  for (let index = 0; index < amount; index += 1) {
+    const box = document.createElement("div");
+    const boxSize = `${firstBoxSize + increaseNextBoxes * index}px`;
+    console.log(boxSize);
+    box.style.backgroundColor = getRandomHexColor();
+    box.style.width = boxSize;
+    box.style.height = boxSize;
+    console.log(box);
+    // boxes = `${boxes}${box}`;
+    boxes.push(box);
+    boxes.join("");
+    console.log(...boxes);
+    boxesEl.append(...boxes);
+  }
+}
